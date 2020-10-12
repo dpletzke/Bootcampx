a94b1f5481adcbf285e4019e6385576e4899e564
Dan Pletzke <dpletzke@gmail.com>
2020-10-12 21:39:35 +0000

    queries intial

---
+++ 2_queries_joins/1_cohorts_total_assign_duration.sql
@@ -0,0 +1,7 @@
+SELECT  SUM(assignment_submissions.duration)
+FROM cohorts
+JOIN students
+ON cohorts.id = students.cohort_id
+JOIN assignment_submissions
+ON assignment_submissions.student_id = students.id
+WHERE cohorts.name = 'FEB12';  
