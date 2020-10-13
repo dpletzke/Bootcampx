7996eb77badff2a31609f254b0f23c2fdd9f0133
Dan Pletzke <dpletzke@gmail.com>
2020-10-13 22:19:23 +0000

    forgot to save solution

--- 4_queries/5_cohort_with_longest_avg_assist_time.sql
+++ 4_queries/5_cohort_with_longest_avg_assist_time.sql
@@ -1,1 +1,10 @@
-select 
+SELECT  cohorts.name
+       ,AVG(completed_at - started_at) AS average_assistance_time
+FROM students
+JOIN cohorts
+ON students.cohort_id = cohorts.id
+JOIN assistance_requests
+ON assistance_requests.student_id = students.id
+GROUP BY  cohorts.name
+ORDER BY average_assistance_time DESC
+LIMIT 1;
